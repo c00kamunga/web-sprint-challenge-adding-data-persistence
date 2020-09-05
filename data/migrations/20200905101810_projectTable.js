8c527e6cf748f3f5eb5796f92fbd72ce7e9905d2
c00kamunga <tsothalanti2@yahoo.com>
2020-09-05 10:48:17 -0700

    added resource table to migration

--- data/migrations/20200905101810_projectTable.js
+++ data/migrations/20200905101810_projectTable.js
@@ -6,11 +6,20 @@ exports.up = async function(knex) {
       tbl.string('description')
       tbl.boolean('completed').defaultTo(false).notNullable()
   })
+
+  await knex.schema.createTable("resource", tbl => {
+      tbl.increments('id')
+      tbl.string('name').notNullable().unique()
+      tbl.string('description')
+  })
+
+
 };
 
 
 
 exports.down = function(knex) {
+await knex.schema.dropTableIfExists('resource')
 await knex.schema.dropTableIfExists('project')    
 };
 
