b56a05055d888c2c441e079ea24afdb4400b650b
c00kamunga <tsothalanti2@yahoo.com>
2020-09-05 11:48:06 -0700

    imported projectRouter into server.js to server.use the projectRouter

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const server = express()
 
 server.use(express.json())
 
+const projectRouter = require('./projectRouter')
+
+server.use(projectRouter)
+
 server.get('/', (req, res) => {
     res.send('<h1>HELLO WELCOME</h1>')
 })
